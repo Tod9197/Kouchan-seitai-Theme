c755adefb3efb5260e537563d771a3be4c6926b1
Tod9197 <dancein18@yahoo.co.jp>
2024-12-04 23:19:04 +0900

    ブログの追加

--- Kouchan-seitai/functions.php
+++ Kouchan-seitai/functions.php
@@ -81,6 +81,46 @@ function custom_post_type_voices(){
   register_taxonomy_for_object_type('post_tag','voices');
 }
 
+//ブログの投稿
+function custom_post_type_blogs(){
+  $labels = array(
+    'name' => 'ブログ',
+    'singular_name' => 'ブログ',
+    'menu_name' => 'ブログ',
+    'name_admin_bar' => 'ブログ',
+    'add_new' => '新規追加',
+    'add_new_item' => '新しいブログを追加',
+    'edit_item' => 'ブログの編集',
+    'new_item' => '新しいブログ',
+    'view_item' => 'ブログを見る',
+    'all_items' => '全てのブログ',
+    'search_items' => 'ブログを検索',
+    'not_found' => 'ブログが見つかりません',
+    'not_found_in_trash' => 'ゴミ箱にブログはありません',
+  );
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_in_menu' => true,
+    'query_var' => true,
+    'rewrite' => [
+      'slug' => 'blogs',
+      'with_front' => false,
+    ],
+    'capability_type' => 'post',
+    'has_archive' => true,
+    'hierarchical' => false,
+    'menu_position' => 7,
+    'supports' => array('title','editor','thumbnail','custom-fields'),
+  );
+  register_post_type('blogs',$args);
+
+  // カテゴリーとタグを追加
+  register_taxonomy_for_object_type('category','blogs');
+  register_taxonomy_for_object_type('post_tag','blogs');
+}
+
 function set_custom_post_type_archive_posts_per_page($query){
   if(!is_admin()  && $query->is_main_query() && is_post_type_archive('voices')){
     $query->set('posts_per_page',12);
@@ -89,6 +129,7 @@ function set_custom_post_type_archive_posts_per_page($query){
 
 add_action('after_setup_theme','mytheme_setup');
 add_action('init','custom_post_type_voices');
+add_action('init','custom_post_type_blogs');
 add_action('pre_get_posts','set_custom_post_type_archive_posts_per_page');
 
 ?>
